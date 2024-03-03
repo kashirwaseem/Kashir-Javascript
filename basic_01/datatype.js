93dfb3e8fa83002e564d2881bf4747c03da8f064
kashirwaseem <65031914+kashirwaseem@users.noreply.github.com>
2024-03-03 10:23:22 +0000

    datatype learning

---
+++ basic_01/datatype.js
@@ -0,0 +1,16 @@
+let name = "Kashir"
+let age = 23
+let male = true
+let married = null
+let nature = undefined
+// number length = 2 to the power 53 
+console.table([name,age,male,married,nature])
+console.log(typeof age);
+/* 
+primitive datatype
+number => 2 to the power 53
+string => "Kashir"
+bigInt = > basically used in long calculation such as tradding and etc 
+null => standalone value and it is type is object 
+symbol => symbol is used for uniqueness 
+
