e05da92d09e4a2e840b257dbded60c1d41959a93
kashirwaseem <65031914+kashirwaseem@users.noreply.github.com>
2024-03-03 09:55:56 +0000

    Variable Learning

---
+++ basic_01/variable.js
@@ -0,0 +1,11 @@
+const accountId = 1245863;
+let accountEmail = "Kashir@gmail.com";
+var accountPassword = "12345";
+accountCity = "Karachi";
+
+// accountId = "123456789"; 
+accountEmail = "Kashirwaseem960@gmail.com";
+accountPassword = "78954612";
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
