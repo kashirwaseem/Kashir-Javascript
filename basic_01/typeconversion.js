44ad09b96aac9ac524fac1dd92e734e26295dd77
kashirwaseem <65031914+kashirwaseem@users.noreply.github.com>
2024-03-03 11:13:10 +0000

    typeconversion Learning

---
+++ basic_01/typeconversion.js
@@ -0,0 +1,57 @@
+// let score = "33abc";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log("Now give the value Null")
+
+// let score1 = null;
+// console.log(typeof score1);
+
+// let valueInNumber1 = Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// console.log("Now Give the value Undefined");
+
+// let score2 = undefined;
+// console.log(typeof score2);
+
+// let valueInNumber2 = Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// console.log("Now Give the value bolean");
+
+// let score3 = true;
+// console.log(typeof score3);
+
+// let valueInNumber3 = Number(score3);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+// console.log("Now Give the value string");
+
+// let score4 = "Kashir";
+// console.log(typeof score4);
+
+// let valueInNumber4 = Number(score4);
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+console.log("now typeconversion of number into string"); 
+let age = 24
+let StringAge = String(age)
+console.log(typeof StringAge);
+console.log(typeof age);
+// let isLogIn = "Kashir" ;
+// console.log(isLogIn);
+
+// let booleanLogIn = Boolean(isLogIn);
+// console.log(booleanLogIn);
+
+// 0 => false , 1 => True 
+// "" => false 
+// "kashir" => true
