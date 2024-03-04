33d7a5e84636ed309276dbea9a9c9805d142b0ca
kashirwaseem <65031914+kashirwaseem@users.noreply.github.com>
2024-03-04 17:34:43 +0000

    Comparison operator Learning

---
+++ basic_01/comparison.js
@@ -0,0 +1,14 @@
+// console.log(1 > 2 );
+// console.log(1 >= 2 );
+// console.log(1 < 2 );
+// console.log( 1 <= 2 );
+
+// console.log("01" > 2 ); // make when doing comparison the typetype should be same 
+// console.log("02" > 1 ); // make when doing comparison the typetype should be same
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0 );
+console.log(null <= 0 );
+// == only check the condition is true or not it will not the check the datatype 
+// === its a strict check it check that the datatype is same or not ?
